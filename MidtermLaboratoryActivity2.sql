87b9b2cd4c78d4ed5650cf4b44f047213d88458c
M-M-I-V <neiljaylacandazo@gmail.com>
2025-02-28 08:38:17 +0800

    Added comments

--- MidtermLaboratoryActivity2.sql
+++ MidtermLaboratoryActivity2.sql
@@ -1,3 +1,4 @@
+-- Create students table
 CREATE TABLE students (
     student_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
@@ -6,15 +7,18 @@ CREATE TABLE students (
     major VARCHAR(50)
 );
 
+-- Create courses table
 CREATE TABLE courses (
     course_id INT AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(100),
     credits INT
 );
 
+-- Set default value for credits column
 ALTER TABLE courses
 ALTER COLUMN credits SET DEFAULT 3;
 
+-- Create enrollments table
 CREATE TABLE enrollments (
     enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
     student_id INT,
@@ -24,6 +28,7 @@ CREATE TABLE enrollments (
     FOREIGN KEY(course_id) REFERENCES courses(course_id)
 );
 
+-- Insert data into students table
 INSERT INTO students(first_name, last_name, date_of_birth, major)
 VALUES ('Neil Jay', 'Lacandazo', '2004-10-20', 'Computer Engineering'),
        ('Joliza Khey', 'Gonzales', '2005-08-15', 'Architecture'),
@@ -31,6 +36,7 @@ VALUES ('Neil Jay', 'Lacandazo', '2004-10-20', 'Computer Engineering'),
        ('Ma. Jenifer', 'Labag', '2004-11-04', 'Information Systems'),
        ('Abegail', 'Zabala', '2002-05-02', 'Computer Science');
 
+-- Insert data into courses table
 INSERT INTO courses(course_name, credits)
 VALUES ('Information Management', 3),  
        ('System Analysis and Design', 3),  
@@ -38,6 +44,7 @@ VALUES ('Information Management', 3),
        ('Data Structures and Algorithms', 3),
        ('Fundamentals of Database Systems', 3);
 
+-- Insert data into enrollments table
 INSERT INTO enrollments(student_id, course_id, enrollment_date)
 VALUES (1, 1, '2025-08-22'),
        (1, 2, '2025-08-22'),
@@ -50,16 +57,18 @@ VALUES (1, 1, '2025-08-22'),
        (5, 4, '2025-08-22'),
        (5, 5, '2025-08-22');
 
+-- Retrieve data from tables
 SELECT * FROM students;
 
 SELECT * FROM courses;
 
 SELECT * FROM enrollments;
 
+-- Retrieve data from tables with specific conditions
 SELECT * FROM students
 WHERE major = 'Computer Science';
 
-
+-- Update and delete data
 UPDATE students
 SET major = 'Information Systems'
 WHERE student_id = 1;
@@ -75,6 +84,7 @@ WHERE enrollment_id = 8;
 DELETE FROM courses
 WHERE course_id = 7;
 
+-- Insert additional data into courses table
 INSERT INTO courses(course_name, credits)
 VALUES ('Computer Programming 2', 3);
 
