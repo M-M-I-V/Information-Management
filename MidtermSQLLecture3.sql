e61a3195ffaf5b50d4407e89aad09d4ad2ce9306
Neil Jay Lacandazo <138986020+M-M-I-V@users.noreply.github.com>
2025-03-04 22:53:12 +0800

    Update MidtermSQLLecture3.sql

--- MidtermSQLLecture3.sql
+++ MidtermSQLLecture3.sql
@@ -1,3 +1,19 @@
+
+-- Create multiple_tables_21 database
 CREATE DATABASE multiple_tables_21;
 
+-- Use multiple_tables_21 database
 USE multiple_tables_21;
+
+-- Create employees_inner table
+CREATE TABLE employees_inner (
+    id INT PRIMARY KEY, 
+    employee_name VARCHAR(50),
+    dept_id INT
+);
+
+-- Create departments_inner table
+CREATE TABLE departments_inner (
+    id INT PRIMARY KEY, 
+    dept_name VARCHAR(50)
+);
